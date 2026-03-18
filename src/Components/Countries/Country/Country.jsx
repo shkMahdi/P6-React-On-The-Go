2b388048f9cdf1d9651882c8cf8734f0ee2ce636
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-03-18 20:18:54 +0600

    added some styling and conditioned rendering

--- src/Components/Countries/Country/Country.jsx
+++ src/Components/Countries/Country/Country.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import './country.css'
 const Country = ({country}) => {
     // console.log(country.name);
     return (
-        <div>
+        <div className='country'>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
-            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <p>Area: {country.area.area} square km  ({country.area.area > 300000 ? 'Big Country' : 'Small Country'})</p>
         </div>
     );
 };
