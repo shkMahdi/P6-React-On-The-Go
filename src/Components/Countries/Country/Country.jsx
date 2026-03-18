ee602eb961d6ef0b7357c3061a17866e489caee6
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2026-03-18 20:33:38 +0600

    conditional css applied based on visited

--- src/Components/Countries/Country/Country.jsx
+++ src/Components/Countries/Country/Country.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import './country.css'
 const Country = ({country}) => {
     // console.log(country.name);
+    const [visited, setVisited] = useState(false);
+    const checkVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-vis'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h4>Name: {country.name.common}</h4>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} square km  ({country.area.area > 300000 ? 'Big Country' : 'Small Country'})</p>
+            <button className='btn-vis' onClick={checkVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
